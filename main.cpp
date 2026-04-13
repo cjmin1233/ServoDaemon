0fa308b27723710a353127e989d8ca18e8572369
cjmin1233 <cjmin1233@gneetech.com>
2026-04-13 11:20:16 +0900

    로그 기능 복구

--- main.cpp
+++ main.cpp
@@ -2,44 +2,6 @@
 #include <QCoreApplication>
 
 #include <iostream>
-// #include <QDir>
-// #include <QFile>
-
-// #include "../CommonConfig.h"
-// #include "ecatserver.h"
-
-#if 0
-/// <summary>
-/// Qt custom message handler for logging
-/// </summary>
-/// <param name="type"> Type of message </param>
-/// <param name="context"> Message context </param>
-/// <param name="msg"> Message content </param>
-void myMessageOutput(QtMsgType type, const QMessageLogContext& context, const QString& msg)
-{
-    // create logs directory if not exists
-    QString logDirPath = QCoreApplication::applicationDirPath() + "/logs";
-    QDir    logDir(logDirPath);
-    if (!logDir.exists()) logDir.mkpath(".");
-
-    // define log file path based on current date
-    QString dateString  = QDateTime::currentDateTime().toString("yyyy-MM-dd");
-    QString logFilePath = logDirPath + QString("/%1_log.txt").arg(dateString);
-
-    QFile outFile(logFilePath);
-    if (outFile.open(QIODevice::WriteOnly | QIODevice::Append)) {
-        QTextStream ts(&outFile);
-        QString     timeStr = QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss.zzz");
-
-        // determine message type string
-        QString typeStr = "INFO ";
-        if (type == QtCriticalMsg || type == QtFatalMsg) typeStr = "ERROR";
-
-        ts << "[" << timeStr << "] [" << typeStr << "] " << msg << Qt::endl;
-        outFile.close();
-    }
-}
-#endif
 
 int main(int argc, char* argv[])
 {
@@ -76,18 +38,4 @@ int main(int argc, char* argv[])
     }
 
     return 0;
-
-    // #ifndef QT_DEBUG
-    //     // install custom message handler for logging
-    //     qInstallMessageHandler(myMessageOutput);
-    // #endif
-
-    //     QCoreApplication a(argc, argv);
-
-    //     qDebug() << "---------- Servo Daemon Started ----------";
-
-    //     EcatServer* server = new EcatServer(&a);
-    //     server->start();
-
-    //     return a.exec();
 }
--- windowsservice.cpp
+++ windowsservice.cpp
@@ -3,10 +3,43 @@
 
 #include <QCoreApplication>
 #include <QDebug>
+#include <QDir>
+#include <QFile>
 
 QString               WindowsService::m_serviceName   = "";
 SERVICE_STATUS_HANDLE WindowsService::m_statusHandle  = nullptr;
 SERVICE_STATUS        WindowsService::m_serviceStatus = { 0 };
+
+/// <summary>
+/// Qt custom message handler for logging
+/// </summary>
+/// <param name="type"> Type of message </param>
+/// <param name="context"> Message context </param>
+/// <param name="msg"> Message content </param>
+void myMessageOutput(QtMsgType type, const QMessageLogContext& context, const QString& msg)
+{
+    // create logs directory if not exists
+    QString logDirPath = QCoreApplication::applicationDirPath() + "/logs";
+    QDir    logDir(logDirPath);
+    if (!logDir.exists()) logDir.mkpath(".");
+
+    // define log file path based on current date
+    QString dateString  = QDateTime::currentDateTime().toString("yyyy-MM-dd");
+    QString logFilePath = logDirPath + QString("/%1_log.txt").arg(dateString);
+
+    QFile outFile(logFilePath);
+    if (outFile.open(QIODevice::WriteOnly | QIODevice::Append)) {
+        QTextStream ts(&outFile);
+        QString     timeStr = QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss.zzz");
+
+        // determine message type string
+        QString typeStr = "INFO ";
+        if (type == QtCriticalMsg || type == QtFatalMsg) typeStr = "ERROR";
+
+        ts << "[" << timeStr << "] [" << typeStr << "] " << msg << Qt::endl;
+        outFile.close();
+    }
+}
 
 WindowsService::WindowsService(const QString& serviceName)
 {
@@ -108,6 +141,11 @@ void WindowsService::serviceMain(DWORD /*argc*/, LPTSTR* /*argv*/)
     QCoreApplication a(argc, argv);
 
     setServiceStatus(SERVICE_RUNNING);
+
+#ifndef QT_DEBUG
+    // install custom message handler for logging
+    qInstallMessageHandler(myMessageOutput);
+#endif
 
     qDebug() << "---------- Servo Daemon Started ----------";
 
